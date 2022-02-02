4e4b441f872476d7cdc36a15a7d3fced08e25d62
Lujie Zhao <lujie.sue.zhao@gmail.com>
2022-02-02 18:51:36 -0500

    Add animation and fix broken Linkedin link

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
+const drops = [];
+const quote = 'KEEP YOUR HEAD IN THE CLOUD.';
+let i = 0;
+let speed = 5000;
+let canvas, headerHeight;
+let h1, h2, section1, section2;
 
+function windowResized() {
+  headerHeight = document.getElementById('header').clientHeight - 65;
+  resizeCanvas(windowWidth, headerHeight);
+
+  section1 = document.getElementById('section1').getBoundingClientRect();
+  section2 = document.getElementById('section2').getBoundingClientRect();
+  // print(headerHeight, section1);
+  h1.position(section1.x, section1.y-10);
+  h2.position(section2.x, section2.y-10);
+}
+
+function setup() {
+  headerHeight = document.getElementById('header').clientHeight - 65;
+  canvas = createCanvas(windowWidth, headerHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index','-3');
+  section1 = document.getElementById('section1').getBoundingClientRect();
+  section2 = document.getElementById('section2').getBoundingClientRect();
+  // print(headerHeight, section1, section2);
+
+  for(let i = 0; i < width; i++) {
+    drops[i] = random(height);
+  }
+  stroke(0);
+}
+
+function draw() {
+  background('#f5f2d5');
+  for(let i = 0; i < drops.length; i++) {
+    drops[i] += random(2);
+    if (drops[i] > height) {
+      drops[i] = 0;
+    }
+    point(i, drops[i]);
+  }
+  typerEffect();
+}
+
+function typerEffect() {
+  if (i < quote.length) {
+    document.getElementById("name").innerHTML += quote.charAt(i);
+    i++;
+    setTimeout(typerEffect, speed*100);
+  }
+}
